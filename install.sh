cb09b7d59f3ec14f0f99391b7687330a9222ea7a
DENNNOUGORILLA <dennougorilla@gmail.com>
2018-01-30 10:20:55 +0900

    [fix] deploy

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+chmod 755 *
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
-    ln -s $HOME/dotfiles/$f $HOME/$f
+    ln -s ${HOME}/dotfiles/${f} ${HOME}/${f}
 done
-
-ln -s $HOME/dotfiles/dein.toml $HOME/dein.toml
+ln -s ${HOME}/dotfiles/dein.toml ${HOME}/dein.toml
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
